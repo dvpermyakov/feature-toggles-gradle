ff2e016cf8831dfe1b819d1c265982eed0d6d302
dmitrii.permiakov <dmitrii.permiakov@revolut.com>
2020-10-04 23:08:10 +0400

    fix test

--- sample/src/test/java/com.dvpermyakov.feature.toggles/CardSampleTest.kt
+++ sample/src/test/java/com.dvpermyakov.feature.toggles/CardSampleTest.kt
@@ -1,14 +1,14 @@
 package com.dvpermyakov.feature.toggles
 
 import com.dvpermyakov.feature.toggles.data.FeatureToggleRepositoryImpl
-import com.dvpermyakov.feature.toggles.domain.FeatureToggleConfig_default
+import com.dvpermyakov.feature.toggles.domain.FeatureToggleConfig_custom
 import org.junit.Assert
 import org.junit.Test
 
 class CardSampleTest {
 
     private val featureToggleRepository = FeatureToggleRepositoryImpl(
-        config = FeatureToggleConfig_default
+        config = FeatureToggleConfig_custom
     )
 
     private val cardSample = CardSample(
